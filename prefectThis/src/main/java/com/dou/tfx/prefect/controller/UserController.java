d7985569f8981a5aa2cbc7803f876b560c0dc173
doubujizi <doubujizi@users.noreply.github.com>
2020-07-20 18:44:46 +0800

    测试Valid校验

--- prefectThis/src/main/java/com/dou/tfx/prefect/controller/UserController.java
+++ prefectThis/src/main/java/com/dou/tfx/prefect/controller/UserController.java
@@ -2,8 +2,13 @@ package com.dou.tfx.prefect.controller;
 
 import com.dou.tfx.prefect.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
 
 /**
  * @author tianfuxian
@@ -12,11 +17,17 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/all/user")
+//@Validated
 public class UserController {
     @Autowired
     private UserService userService;
 
     public void aaa() {
         System.out.println(1111);
+    }
+
+    @PostMapping("/testValid")
+    public String testValid(@Valid @NotBlank Integer number) {
+        return "hello";
     }
 }
